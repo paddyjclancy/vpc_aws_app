666e253fef2b6a100725c4289e721b0f02e9d76d
paddyjclancy <patrickclancy0605@gmail.com>
2020-07-08 17:24:04 +0100

    jenkins provision

---
+++ environment/app/j-provision.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Install JDK (Java Development Kit)
+sudo apt update
+sudo apt install default-jdk
+java -version
+
+# Install Jenkins
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > \
+    /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install jenkins
